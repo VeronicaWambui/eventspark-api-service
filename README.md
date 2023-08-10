e8a95d473568e10c76d7e2cc77dc6222bad3e9e3
Veronica Wambui <thisisveronicawambui@gmail.com>
2023-08-10 17:23:24 +0300

    Final draft; added comments and edited README.md

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# EventSpark
+# EventSpark-API-service
+The following is the API service to consume as proxy in the frontend. Written in the Flask python framework.
+
+## Project Initialization
+To run the project. Navigate to the root of the directory. Activate the virtual environment. For Mac Users type `source ./venv/bin/activate` to activate the virtual environment.
+Then run `flask run`
+To view the deployment live. 
+Open [https://eventspark-api-service.onrender.com/](https://eventspark-api-service.onrender.com/)
+
+## Team
+Research and development by Veronica Wambui Wanjiku.
+ALX-software engineering programme.
+&copy; 2023.
--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+"""
+    Project: Eventspark API service.
+    Author : Veronica Wambui Wanjiku.
+    Description: API service for an event management application.
+    August 2023 - Project done in fulfilment of the ALX software engineering programme.
+    Portfolio Project.
+"""
+#Application based imports.
 from flask import Flask, request, jsonify, make_response
-from flask.helpers import send_from_directory
 from flask_sqlalchemy import SQLAlchemy
+from typing import TypedDict
 import jwt
 from functools import wraps
 import uuid
@@ -8,14 +16,20 @@ import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_cors import CORS
 
+
+# Initialize Flask application
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] =  "ALX_APP_eventspark"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-db = SQLAlchemy(app)
-CORS(app)
+app.config['SECRET_KEY'] =  "ALX_APP_eventspark" #Secret key configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db' #Database configuration.
+db = SQLAlchemy(app) # Initialize database
+CORS(app) # Handle Cross-Origin request policy.
 
 class User(db.Model):
+    """
+    @USER model
+    @PROPERTIES public_id | name | password | role
+    """
     id = db.Column(db.Integer, primary_key=True)
     public_id = db.Column(db.String(50), unique=True)
     name = db.Column(db.String(50))
@@ -23,6 +37,10 @@ class User(db.Model):
     admin = db.Column(db.Boolean)
 
 class Event(db.Model):
+    """
+    @EVENT model
+    @PROPERTIES event_id | name | description.
+    """
     id = db.Column(db.Integer, primary_key=True)
     event_id = db.Column(db.String(50), unique=True)
     name = db.Column(db.String(50))
@@ -30,6 +48,7 @@ class Event(db.Model):
 
 
 def token_required(f):
+    """@Decorator to restrict specific route access."""
     @wraps(f)
     def check_auth(*args, **kwargs):
         token = None
@@ -47,11 +66,17 @@ def token_required(f):
     return check_auth
 
 
-@app.route('/api/v1//user', methods=['POST'])
-@token_required
+"""USER routes"""
+@app.route('/api/v1/user', methods=['POST'])
+# @token_required
 def create_user(current_user):
-    if not current_user.admin:
-        return jsonify({'message': 'Cannot perform that function'})
+    """
+    @POSTrequest
+    Creates a new user to the database
+    @properties: public_id | name | password | role
+    """
+    # if not current_user.admin:
+    #     return jsonify({'message': 'Cannot perform that function'})
     data = request.get_json()
     hashed_password  = generate_password_hash(data['password'], method='pbkdf2')
     new_user = User(public_id=str(uuid.uuid4()), name=data['name'], password=hashed_password, admin=False)
@@ -59,8 +84,13 @@ def create_user(current_user):
     db.session.commit()
     return jsonify({'status': 'success', 'message': 'New user created'})
 
-@app.route('/api/v1//user', methods=['GET'])
+@app.route('/api/v1/user', methods=['GET'])
 def get_all_users():
+    """
+    @GET request
+    Gets all the users from the database.
+    @Returns all users.
+    """
     users = User.query.all()
     output = []
 
@@ -75,8 +105,13 @@ def get_all_users():
     return jsonify({'users': output})
 
 
-@app.route('/api/v1//user/<public_id>', methods=['GET'])
+@app.route('/api/v1/user/<public_id>', methods=['GET'])
 def get_single_user(public_id):
+    """
+    @GET request
+    Retrieves a user from the database based on the id.
+    @properties: public_id
+    """
     user = User.query.filter_by(public_id=public_id).first()
 
     if not user:
@@ -90,8 +125,13 @@ def get_single_user(public_id):
 
     return jsonify({'user': user_data})
 
-@app.route('/api/v1//user/<public_id>', methods=['PUT'])
+@app.route('/api/v1/user/<public_id>', methods=['PUT'])
 def elevate_user(public_id):
+    """
+    @PUT request
+    Gives user elevated access as administrator
+    @properties: public_id
+    """
     user = User.query.filter_by(public_id=public_id).first()
     if not user:
         return jsonify({'message': 'No user found!'})
@@ -100,8 +140,13 @@ def elevate_user(public_id):
 
     return jsonify({'message': 'The user has been promoted!'})
 
-@app.route('/api/v1//user/<public_id>', methods=['DELETE'])
+@app.route('/api/v1/user/<public_id>', methods=['DELETE'])
 def delete_user(public_id):
+    """
+    @DELETE request
+    Removes a user from the database based on the id.
+    @properties: public_id
+    """
     user = User.query.filter_by(public_id=public_id).first()
     if not user:
         return jsonify({'message': 'No user found!'})
@@ -111,9 +156,14 @@ def delete_user(public_id):
 
     return jsonify({'message': f'user with id {user.public_id} deleted successfully!'})
 
-
-@app.route('/api/v1//login')
+"""Authentication Routes"""
+@app.route('/api/v1/login')
 def login():
+    """
+    @POST request
+    Provides authorization token for authentication
+    @returns a token
+    """
     auth = request.authorization
     if not auth or not auth.username or not auth.password:
         return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
@@ -130,8 +180,13 @@ def login():
 
 
 
+"""EVENT routes"""
 @app.route('/api/v1/event', methods=['POST'])
 def publish_event():
+    """@POST request
+        Creates a new event with a unique ID
+        @properties: name | description
+    """
     data = request.get_json()
     new_event = Event(event_id=str(uuid.uuid4()), name=data['name'], description=data['description'])
     db.session.add(new_event)
@@ -140,6 +195,10 @@ def publish_event():
 
 @app.route('/api/v1/event', methods=['GET'])
 def get_all_events():
+    """@GET request
+        Returns all event listings
+        @properties: event_id | name | description
+    """
     events = Event.query.all()
 
     output = []
@@ -155,6 +214,10 @@ def get_all_events():
 
 @app.route('/api/v1/event/<event_id>', methods=['GET'])
 def get_single_event(event_id):
+    """@GET request
+        Returns a single event given its ID.
+        @properties: event_id | name | description
+    """
     event = Event.query.filter_by(event_id=event_id).first()
 
     if not event:
@@ -171,6 +234,10 @@ def get_single_event(event_id):
 
 @app.route('/api/v1/event/<event_id>', methods=['PUT'])
 def edit_published_event(event_id):
+    """@PUT request
+        Modifies data for a single event given its ID.
+        @properties: event_id | name | description
+    """
     event = Event.query.filter_by(event_id=event_id).first()
 
     if not event:
@@ -187,8 +254,11 @@ def edit_published_event(event_id):
 @app.route('/api/v1/event/<event_id>', methods=['DELETE'])
 # @token_required
 def delete_event(event_id):
+    """@DELETE request
+        Removes an event from the database given its id
+        @properties: event_id 
+    """
     event = Event.query.filter_by(event_id=event_id).first()
-
     if not event:
         return jsonify({'message' : f'No event listed with id {event.event_id}!'})
 
@@ -200,8 +270,13 @@ def delete_event(event_id):
 
 @app.route('/')
 def root():
-    return jsonify({'message': "Welcome to the eventspark api."})
-
+    """Root"""
+    return jsonify({'root': {
+        "Project": "Eventspark-api-service", 
+        "Author": "Veronica Wambui Wanjiku.", 
+        "Programme": "ALX Software Engineering Programme",
+        "version": "0.1.0.0"
+    }})
 
 
 if __name__ == '__main__':
